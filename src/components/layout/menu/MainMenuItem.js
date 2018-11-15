dcc1bc2a3ddf0a579c3c22051b9eea4999346b74
ALBERTO JESUS <alu0100949568@cc1214.etsii.ull.es>
2018-11-15 09:50:14 +0000

    Change SimpleMenu to MenuList

--- src/components/layout/menu/MainMenuItem.js
+++ src/components/layout/menu/MainMenuItem.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from '@material-ui/core/es/Menu/Menu';
+import PropTypes from 'prop-types';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuList from '@material-ui/core/MenuList';
+import { withStyles } from '@material-ui/core/styles';
 
 class MainMenuItem extends Component {
   state = {
     anchorEl: null,
+    open: false
   };
 
   handleClick = event => {
     this.setState({ anchorEl: event.currentTarget });
+    this.setState(state => ({ open: !state.open }));
   };
 
   handleClose = () => {
@@ -19,31 +28,46 @@ class MainMenuItem extends Component {
   render() {
     const { anchorEl } = this.state;
     const { options, title } = this.props;
+    const { open } = this.state;
 
     return (
         <div>
-          <Button
-              aria-owns={anchorEl ? 'simple-menu' : undefined}
-              aria-haspopup="true"
-              onClick={this.handleClick}
-          >
+        <Button
+           buttonRef={node => {
+             this.anchorEl = node;
+           }}
+           aria-owns={open ? 'menu-list-grow' : null}
+           aria-haspopup="true"
+           onClick={this.handleClick}
+         >
             {title}
           </Button>
-          <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.handleClose}
-          >
-            {options.map(option => (
-                <MenuItem
-                    key={option.id}
-                    id={option.id}
-                    onClick={this.handleClose}
-                >
-                  {option.name}
-                </MenuItem>
-            ))}
-          </Menu>
+          <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+           {({ TransitionProps, placement }) => (
+             <Grow
+               {...TransitionProps}
+               id="menu-list-grow"
+               style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+             >
+               <Paper>
+                 <ClickAwayListener onClickAway={this.handleClose}>
+                   <MenuList>
+                   {options.map(option => (
+                       <MenuItem
+                           key={option.id}
+                           id={option.id}
+                           onClick={this.handleClose}
+                       >
+                         {option.name}
+                       </MenuItem>
+                   ))}
+                   </MenuList>
+                 </ClickAwayListener>
+               </Paper>
+             </Grow>
+           )}
+         </Popper>
+
         </div>
     );
   }
