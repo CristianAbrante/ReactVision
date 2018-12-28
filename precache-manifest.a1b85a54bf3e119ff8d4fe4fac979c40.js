1d31cdbad1a5fe03c60a5c104f49c651d838d8d1
Cristian Abrante <cristianabrantedorta@gmail.com>
2018-12-28 00:09:42 +0000

    Updates

---
+++ precache-manifest.a1b85a54bf3e119ff8d4fe4fac979c40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27bfeb2978ebd96b8410",
+    "url": "/ReactVision/static/js/runtime~main.27bfeb29.js"
+  },
+  {
+    "revision": "6c29b752718c7f92c83d",
+    "url": "/ReactVision/static/js/main.6c29b752.chunk.js"
+  },
+  {
+    "revision": "2d957e0269ec31712ed7",
+    "url": "/ReactVision/static/js/1.2d957e02.chunk.js"
+  },
+  {
+    "revision": "6c29b752718c7f92c83d",
+    "url": "/ReactVision/static/css/main.228a1485.chunk.css"
+  },
+  {
+    "revision": "7a2124b610dab4e3b148a37d817cc99b",
+    "url": "/ReactVision/index.html"
+  }
+];
